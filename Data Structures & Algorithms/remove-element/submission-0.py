f6117a2dff3bb890a4bd9c34dbd59ab02d2706c6
Abdulaziz Omonov <abdulaziomonov4@gmail.com>
2026-07-03 08:48:42 +0500

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        slow = 0
+
+        for fast in nums:
+            if fast != val:
+                nums[slow]=fast
+                slow+=1
+        return slow
