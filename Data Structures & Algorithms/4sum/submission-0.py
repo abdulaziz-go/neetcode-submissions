96804913e7cedaf7f4003e8faf4918d8745d4a42
Abdulaziz Omonov <abdulaziomonov4@gmail.com>
2026-07-25 08:20:36 +0500

    Add: 4sum - submission-0

---
+++ Data Structures & Algorithms/4sum/submission-0.py
@@ -0,0 +1,29 @@
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+            # i j l->  <-r
+        # [-3,0,1,2,3,3]
+        nums.sort()
+        result = []
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            for j in range(i + 1, len(nums)):
+                if j - 1 != i and nums[j] == nums[j-1]:
+                    continue
+                left = j + 1
+                right = len(nums) - 1
+                while left < right:
+                    summ = nums[i] + nums[j] + nums[left] + nums[right]
+                    if summ > target:
+                        right -= 1
+                    elif summ < target:
+                        left += 1
+                    else:
+                        result.append([nums[i], nums[j], nums[left], nums[right]])
+                        left += 1
+                        right -=1
+                        while left < right and nums[left] == nums[left - 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right + 1]:
+                            right -=1
+        return result
